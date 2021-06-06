0552af9f1837c571f3a16825ee4126381b2a02e4
Sonu Yadav <sonukyadav2510@gmail.com>
2021-06-06 22:57:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,4 +21,18 @@ document.querySelector('form').addEventListener('submit', event => {
   const linkInput = document.querySelector('#link-input');
   linkInput.value = `${window.location}#${encrypted}`;
   linkInput.select();
+  function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
 });
